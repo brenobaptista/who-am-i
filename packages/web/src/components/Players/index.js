07170caf2eb67dd0ab0b733c1149da2ab14dd5f9
Breno Baptista <brenobaptista@protonmail.com>
2021-11-19 19:46:10 -0300

    Created component Players

---
+++ packages/web/src/components/Players/index.js
@@ -0,0 +1,57 @@
+import { useState, useEffect, useContext } from 'react'
+
+import styles from './index.module.css'
+import { SocketContext } from '../../contexts/socket'
+
+const Players = () => {
+  const socket = useContext(SocketContext)
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    socket.on('users', users => {
+      const modifiedUsers = [...users]
+
+      modifiedUsers.forEach(user => {
+        const isSelf = user.userId === socket.id
+
+        user.self = isSelf
+      })
+
+      setUsers(modifiedUsers)
+    })
+
+    socket.on('user connected', user => {
+      setUsers(previousState => [...previousState, user])
+    })
+
+    socket.on('user disconnected', id => {
+      setUsers(previousState =>
+        previousState.filter(user => user.userId !== id)
+      )
+    })
+
+    return () => {
+      socket.off('users')
+      socket.off('user connected')
+      socket.off('user disconnected')
+    }
+  }, [])
+
+  return (
+    <>
+      <h1 className={styles.title}>
+        <span>Players</span>
+      </h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.userId} className={user.self ? styles.self : undefined}>
+            {user.username}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Players
--- packages/web/src/containers/Gameplay/index.js
+++ packages/web/src/containers/Gameplay/index.js
@@ -1,62 +1,12 @@
-import { useState, useEffect, useContext } from 'react'
-
+import Players from '../../components/Players'
 import styles from './index.module.css'
-import { SocketContext } from '../../contexts/socket'
 
-const Gameplay = () => {
-  const socket = useContext(SocketContext)
-
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    socket.on('users', users => {
-      const modifiedUsers = [...users]
-
-      modifiedUsers.forEach(user => {
-        const isSelf = user.userId === socket.id
-
-        user.self = isSelf
-      })
-
-      setUsers(modifiedUsers)
-    })
-
-    socket.on('user connected', user => {
-      setUsers(previousState => [...previousState, user])
-    })
-
-    socket.on('user disconnected', id => {
-      setUsers(previousState =>
-        previousState.filter(user => user.userId !== id)
-      )
-    })
-
-    return () => {
-      socket.off('users')
-      socket.off('user connected')
-      socket.off('user disconnected')
-    }
-  }, [])
-
-  return (
-    <div className={styles.container}>
-      <div>
-        <h1 className={styles.title}>
-          <span>Gameplay</span>
-        </h1>
-        <ul>
-          {users.map(user => (
-            <li
-              key={user.userId}
-              className={user.self ? styles.self : undefined}
-            >
-              {user.username}
-            </li>
-          ))}
-        </ul>
-      </div>
+const Gameplay = () => (
+  <div className={styles.container}>
+    <div>
+      <Players />
     </div>
-  )
-}
+  </div>
+)
 
 export default Gameplay

67386636b9206eef3e1ed63b62217051a20fdee1
Breno Baptista <brenobaptista@protonmail.com>
2021-11-19 20:42:29 -0300

    Added Gameplay component

---
+++ packages/web/src/components/Character/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+import styles from './index.module.css'
+
+const Character = () => {
+  const [character, setCharacter] = useState('')
+
+  const handleCharacter = event => {
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleCharacter} className={styles.form}>
+      <div className={styles.title}>Select a famous character</div>
+      <input
+        required
+        type='text'
+        placeholder='Albert Einstein'
+        value={character}
+        onChange={event => setCharacter(event.target.value)}
+        className={styles.input}
+      />
+      <button type='submit' className={styles.button}>
+        Play
+      </button>
+    </form>
+  )
+}
+
+export default Character
--- packages/web/src/containers/Gameplay/index.js
+++ packages/web/src/containers/Gameplay/index.js
@@ -1,9 +1,11 @@
 import Players from '../../components/Players'
+import Character from '../../components/Character'
 import styles from './index.module.css'
 
 const Gameplay = () => (
   <div className={styles.container}>
     <div>
+      <Character />
       <Players />
     </div>
   </div>

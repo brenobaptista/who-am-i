db531070995e3131a344e68f69491ff04972ca39
Breno Baptista <brenobaptista@protonmail.com>
2021-11-25 12:09:13 -0300

    Added env variables to client

--- packages/web/src/contexts/socket.js
+++ packages/web/src/contexts/socket.js
@@ -1,7 +1,8 @@
 import { createContext } from 'react'
 import io from 'socket.io-client'
 
-const url = 'http://localhost:8080'
+// eslint-disable-next-line no-undef
+const url = `${process.env.REACT_APP_SERVER_URL || 'http://localhost:8080'}`
 export const socket = io(url, { autoConnect: false })
 
 export const SocketContext = createContext()

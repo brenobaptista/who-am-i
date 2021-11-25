4e8401fba0b91dc9be4d782a6f9f868fa1a95c2b
Breno Baptista <brenobaptista@protonmail.com>
2021-11-25 12:09:08 -0300

    Added env variables to server

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -2,9 +2,9 @@ import events from './events/index.js'
 import socketIOServer from './loaders/socket.io-server.js'
 import middlewares from './middlewares/index.js'
 
-const io = socketIOServer(8080, {
+const io = socketIOServer(process.env.PORT || 8080, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: `${process.env.CLIENT_URL || 'http://localhost:3000'}`,
     methods: ['GET', 'POST']
   }
 })

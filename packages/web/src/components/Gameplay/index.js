fed75daab19fa3bf8b09f6e6113f21e622cc4916
Breno Baptista <brenobaptista@protonmail.com>
2021-11-25 01:01:29 -0300

    Added Gameplay component

--- packages/web/src/components/Gameplay/index.js
+++ packages/web/src/components/Gameplay/index.js
@@ -4,7 +4,71 @@ import styles from './index.module.css'
 import { SocketContext } from '../../contexts/socket'
 
 const Gameplay = ({ participantPlayers }) => {
-  
+  const socket = useContext(SocketContext)
+
+  const [players, setPlayers] = useState([])
+  const [characterAssigned, setCharacterAssigned] = useState('')
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  useEffect(() => {
+    const playersWithSelf = participantPlayers.map(player => {
+      const isSelf = player.id === socket.id
+
+      const playerWithSelf = { ...player, self: isSelf }
+
+      return playerWithSelf
+    })
+
+    const index = playersWithSelf.findIndex(player => player.id === socket.id)
+
+    setCharacterAssigned(playersWithSelf[index].characterAssigned)
+
+    playersWithSelf[index].characterAssigned = ''
+
+    setPlayers(playersWithSelf)
+  }, [])
+
+  useEffect(() => {
+    socket.on('game over', id => {
+      setPlayers(previousState =>
+        previousState.map(player =>
+          player.id === id ? { ...player, gameOver: true } : { ...player }
+        )
+      )
+    })
+
+    return () => {
+      socket.off('game over')
+    }
+  }, [])
+
+  const handleGameOver = () => {
+    setIsGameOver(true)
+
+    socket.emit('game over')
+  }
+
+  return (
+    <>
+      <div className={styles.title}>Now, who am I?</div>
+      {players.map(player => (
+        <div key={player.id} className={player.self ? styles.bold : undefined}>
+          <span className={player.gameOver ? styles.lineThrough : undefined}>
+            {player.nickname}
+            {player.characterAssigned && ` (${player.characterAssigned})`}
+          </span>
+        </div>
+      ))}
+      <button type='button' className={styles.button} onClick={handleGameOver}>
+        Game Over
+      </button>
+      {isGameOver && (
+        <p>
+          You are <span className={styles.bold}>{characterAssigned}</span>
+        </p>
+      )}
+    </>
+  )
 }
 
 export default Gameplay
